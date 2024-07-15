b6c86dde9876e372d4d0cf3528fdeaf3084b305e
zhaishuaishuai <zhaishuaishuai@htyg.com.cn>
2024-07-15 15:49:06 +0800

    feat:集成druid和dynamic支持多数据源切换

--- src/test/java/org/example/bms/BmsApplicationTests.java
+++ src/test/java/org/example/bms/BmsApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+//import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -24,7 +26,13 @@ class BmsApplicationTests {
         System.out.println(dataSource.getClass());
         Connection connection = dataSource.getConnection();
         System.out.println(connection);
+        System.out.println(((DynamicRoutingDataSource) dataSource).getDataSources());
         connection.close();
+
+//        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+//        int minIdle = druidDataSource.getMinIdle();
+//        System.out.println("Actual minimum idle connections in Druid pool:" + minIdle);
+
         String sql = "select * from author";
         List<Map<String, Object>> list_maps = jdbcTemplate.queryForList(sql);
         System.out.println(list_maps);
