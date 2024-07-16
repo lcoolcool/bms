93fd8a8bd06589ecbf88acab9563b6a315fbbfbc
zhaishuaishuai <zhaishuaishuai@htyg.com.cn>
2024-07-16 20:48:25 +0800

    update

--- src/main/java/org/example/bms/dto/common/Response.java
+++ src/main/java/org/example/bms/dto/common/Response.java
@@ -17,18 +17,18 @@ public class Response <T> {
         this.code = code;
         this.message = message;
     }
-    public Response(T data, int code, String message) {
+    public Response(int code, String message, T data) {
         this.data = data;
         this.code = code;
         this.message = message;
     }
 
     public static <V> Response<V> success(){
-        return new Response<>(200, "success");
+        return new Response<>(20000, "success");
     }
 
     public static <V> Response<V> success(V data){
-        return new Response<>(data, 20000, "success");
+        return new Response<>(20000, "success", data);
     }
 
     public static <V> Response<V> fail(){
@@ -39,4 +39,19 @@ public class Response <T> {
         return new Response<>(50000, message);
     }
 
+    public static <V> Response<V> fail(int code, String message){
+        return new Response<>(code, message);
+    }
+
+    public static <V> Response<V> fail(int code, String message, V data){
+        return new Response<>(code, message, data);
+    }
+
+    public boolean isOk(){
+        return this.code == 20000;
+    }
+
+    public boolean isFail(){
+        return this.code != 20000;
+    }
 }
